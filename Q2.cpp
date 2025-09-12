c3b5daa2041383f0d8010e108fc7c1cc5dec3504
mems250005035 <mems250005035@iiti.ac.in>
2025-09-12 17:44:30 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -8,11 +8,23 @@ int[newsize];
  for (int i = 0; i < size1; i++) {
     result[i] = arr1[i] ;
  }
-for (int j= 0; j < size2; j++) {
-result[size1 + j] = arr2[j];
+ for (int j= 0; j < size2; j++) {
+ result[size1 + j] = arr2[j];
+ }
+ return result;
 }
 
+int main() {
+  int arr1[] = {1, 2, 3};
+  int size1 size of(arr1) /
+sizeof(arr1[0]);
 
+  int arr2[] = {4, 5, 6, 7};
+  int size2 = sizeof(arr2) /
+sizeof(arr2[0]);
+
+ int newsize;
+ int*
 
 
 
