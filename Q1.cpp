7566fd5dc4615efd41f876ab441958ee1e553d1a
mems250005035 <mems250005035@iiti.ac.in>
2025-09-12 16:48:26 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,6 +1,50 @@
 //  Write a program in C++ with a function called add to add two complex numbers and another function called subtract to subtract one complex number from another. 
+#include <iostream>
+using namespace std;
+struct Complex{
+    float real;
+    float imag;
+};
+Complex add(Complex c1 , Complex c2) {
+  Complex result;
+  result.real = c1.real + c2.real;
+  result.imag = c1.imag + c2.imag;
+  return result;
+}
+Complex subtract(Complex c1 , Complex c2) {
+  Complex result;
+  result.real = c1.real - c2.real;
+  result.imag = c1.imag - c2.imag;
+  return result;
+}
+void display(Complex c) {
+  if (c.imag >= 0)
+    cout << c.real << " + " << c.imag << "i";
+  else
+    cout << c.real << " - " << -c.imag << "i";
+}
 
+int main() {
+  Complex num1 , num2 , sum , diff ;
 
+ cout << "Enter real and imaginary part of first complex number: ";
+ cin >> num1.real >> num1.imag;
+
+ cout << "Enter real and imaginary part of second complex number: ";
+ cin >> num2.real >> num2.imag;
+
+ sum = add(num1 , num2);
+ diff = subtract(num1 , num2);
+ 
+ cout << "\nSum =  ";
+ display(sum);
+ cout << "\nDifference = ";
+ display(diff);
+
+ cout << endl;
+
+ return 0;
+}
 
 
 
